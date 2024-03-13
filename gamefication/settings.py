242081f2c09eea776579171c9fa105542b368c5d
MikasaAl4naya <oleg.pytin@gmail.com>
2024-03-14 04:29:43 +0900

    dotenv added

--- gamefication/settings.py
+++ gamefication/settings.py
@@ -81,7 +81,8 @@ WSGI_APPLICATION = 'gamefication.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
