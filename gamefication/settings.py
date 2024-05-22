c2da909521770bddf1fde835f1466fdde5b24b94
MikasaAl4naya <oleg.pytin@gmail.com>
2024-05-22 10:18:00 +0800

    Список тестов на модерации

--- gamefication/settings.py
+++ gamefication/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['oputintsev.pythonanywhere.com','127.0.0.1']
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'gamefication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oputintsev$gamificationBASE',
+        'USER': 'oputintsev',
+        'PASSWORD': 'Oleg.iori1',
+        'HOST': 'oputintsev.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -125,7 +128,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = '/home/oputintsev/static'
 # Media files (images, videos, etc.)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'C:/Users/olegp/PycharmProjects/DjangoProjects/gamefication/media/'
+MEDIA_ROOT = '/home/oputintsev/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
