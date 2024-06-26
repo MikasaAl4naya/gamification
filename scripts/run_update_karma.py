a9af7e38d8ef059618c2b74d817b0845deb3fe32
MikasaAl4naya <oleg.pytin@gmail.com>
2024-06-27 02:44:59 +0800

    Добавил загрузку файла

--- scripts/run_update_karma.py
+++ scripts/run_update_karma.py
@@ -1,14 +1,15 @@
-import django
 import os
 import sys
+import django
 
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_path)
+print("Project path:", project_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamefication.settings')
+print("DJANGO_SETTINGS_MODULE:", os.environ.get('DJANGO_SETTINGS_MODULE'))
 django.setup()
 
-from tasks import update_employee_karma
+from tasks import run_update
 
-file_path = '/home/Solevoi/work_schedule/Табель тест.xlsx'
-update_employee_karma(file_path)
+run_update('work_schedule')
