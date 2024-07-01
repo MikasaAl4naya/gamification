b5ed069060f73dd3bf5b553de70161d7a46c56cb
MikasaAl4naya <oleg.pytin@gmail.com>
2024-07-01 11:25:28 +0800

    Добавил загрузку файла

--- main/permissions.py
+++ main/permissions.py
@@ -2,7 +2,20 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='Администраторы').exists()
+        is_authenticated = request.user.is_authenticated
+        is_admin = request.user.groups.filter(name='Администраторы').exists()
+        has_permission = is_authenticated and is_admin
+
+        # Логируем информацию о пользователе и его роли
+        user_info = {
+            "username": request.user.username,
+            "is_authenticated": is_authenticated,
+            "is_admin": is_admin,
+            "groups": list(request.user.groups.values_list('name', flat=True))
+        }
+        print(f"User info: {user_info}")
+
+        return has_permission
 
 class IsModerator(BasePermission):
     def has_permission(self, request, view):
