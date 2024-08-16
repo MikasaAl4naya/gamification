2739b08ba02ee3bf3fd13c02b427639a24502b09
MikasaAl4naya <oleg.pytin@gmail.com>
2024-08-16 14:57:53 +0800

    Много чего изменил

--- main/admin.py
+++ main/admin.py
@@ -15,8 +15,28 @@ admin.site.register(Permission)
 admin.site.register(FilePath)
 admin.site.register(Feedback)
 admin.site.register(SystemSetting)
+
+
 @admin.register(KarmaSettings)
-class KarmaSettingsAdmin(admin.ModelAdmin):
-    list_display = ('feedback_type', 'level', 'karma_change')
-    list_filter = ('feedback_type', 'level')
-    search_fields = ('feedback_type', 'level')
+class OperationSettingsAdmin(admin.ModelAdmin):
+    list_display = ('operation_type', 'level', 'karma_change', 'experience_change')
+    list_filter = ('operation_type', 'level')
+    search_fields = ('operation_type', 'level')
+
+    def get_list_display(self, request):
+        # Если тип операции - не фидбек, не показываем уровень
+        if any([self.get_queryset(request).filter(operation_type__in=[KarmaSettings.PRAISE, KarmaSettings.COMPLAINT]).exists()]):
+            return ('operation_type', 'level', 'karma_change', 'experience_change')
+        return ('operation_type', 'karma_change', 'experience_change')
+
+    def get_list_filter(self, request):
+        # Если тип операции - не фидбек, не фильтруем по уровню
+        if any([self.get_queryset(request).filter(operation_type__in=[KarmaSettings.PRAISE, KarmaSettings.COMPLAINT]).exists()]):
+            return ('operation_type', 'level')
+        return ('operation_type',)
+
+    def get_search_fields(self, request):
+        # Если тип операции - не фидбек, не ищем по уровню
+        if any([self.get_queryset(request).filter(operation_type__in=[KarmaSettings.PRAISE, KarmaSettings.COMPLAINT]).exists()]):
+            return ('operation_type', 'level')
+        return ('operation_type',)
